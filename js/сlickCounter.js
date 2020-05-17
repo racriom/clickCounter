0e48aed6fa3dbc03058b25bf659a3d212b7bcdba
racriom <58955247+racriom@users.noreply.github.com>
2020-05-17 21:21:37 +0300

    Update сlickCounter.js

--- js/сlickCounter.js
+++ js/сlickCounter.js
@@ -3,5 +3,6 @@ var clicksEl = document.getElementById("clicks");
 function onClick() {
     clicks += 1;
     clicksEl.innerHTML = clicks;
+    document.getElementById("clicker").onclick = onClick
 
 };
