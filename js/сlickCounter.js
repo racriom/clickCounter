4a8df3cccbc5f7c2c077fbac2cf6aa6a99f0af44
racriom <58955247+racriom@users.noreply.github.com>
2020-05-17 22:11:35 +0300

    Delete сlickCounter.js

--- js/сlickCounter.js
+++
@@ -1,8 +0,0 @@
-var clicks = 0;
-var clicksEl = document.getElementById("clicks");
-function onClick() {
-    clicks += 1;
-    clicksEl.innerHTML = clicks;
-    document.getElementById("clicker").onclick = onClick
-
-};
